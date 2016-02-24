1ec422f655e62b867355936e602bd547cdd9ead3
JBar87 <josephbarlan@Josephs-MacBook-Pro.local>
2016-02-23 20:12:34 -0500

    hash_equals compatibility fix

--- reuse_repair_api/app/reuseapp/Helpers/Hash.php
+++ reuse_repair_api/app/reuseapp/Helpers/Hash.php
@@ -5,6 +5,20 @@ namespace Reuseapp\Helpers;
 /**
  * for hashing passwords
  */
+
+if(!function_exists('hash_equals')) {
+  function hash_equals($str1, $str2) {
+    if(strlen($str1) != strlen($str2)) {
+      return false;
+    } else {
+      $res = $str1 ^ $str2;
+      $ret = 0;
+      for($i = strlen($res) - 1; $i >= 0; $i--) $ret |= ord($res[$i]);
+      return !$ret;
+    }
+  }
+}
+        
 class Hash
 {
     protected $config;
@@ -38,8 +52,7 @@ class Hash
 
     public function hashCheck($known, $user)
     {
-        //return hash_equals($known, $user);
-        return ($known === $user);
+        return hash_equals($known, $user);
     }
 
 
